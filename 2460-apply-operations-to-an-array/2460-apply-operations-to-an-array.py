412809ac433278a945c254dceaf8d934ef737c6c
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-01-13 17:19:12 +0300

    Time: 49 ms (95.52%), Space: 14.1 MB (95.73%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        index = 0
+        length = len(nums)
+        while index < length - 1:
+            if nums[index] != 0 and nums[index] == nums[index+1]:
+                nums[index] *= 2
+                nums[index+1] = 0
+                index += 2
+            else:
+                index += 1
+        left = 0
+        right =  0
+        while right < length:
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right += 1
+            else:
+                right += 1
+        return nums
+        
