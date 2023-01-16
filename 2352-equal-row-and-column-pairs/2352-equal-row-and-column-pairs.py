e06831c62b7417806a4e619169b82aecf0979127
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-01-16 09:34:55 +0300

    Time: 515 ms (89.89%), Space: 18.8 MB (91.15%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,4 +1,4 @@
-import json, collections
+import collections
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         rows = collections.defaultdict(int)
@@ -6,13 +6,13 @@ class Solution:
         cols = []
         count = 0
         for i in range(length):
-            rows[json.dumps(grid[i])] += 1
+            rows[tuple(grid[i])] += 1
         for col in range(length):
             column = []
             for row in range(length):
                 column.append(grid[row][col])
-            dumped = json.dumps(column)
-            if rows.get(dumped,-1) != -1:
-                count += rows [dumped]   
+            column = tuple(column)
+            if rows.get(column,-1) != -1:
+                count += rows [column]   
         
         return count
