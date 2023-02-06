a1d16dae4e488f343808c90ff1835d27b9e7d48e
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-02-06 21:42:08 +0300

    Time: 74 ms (57.50%), Space: 15.4 MB (7.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashTable = {}
-        for indx, num in enumerate(nums):
-            if target - num in hashTable:
-                return [indx, hashTable[target - num]]
+        nums = sorted(list(enumerate(nums)), key = lambda x : x[1])
+        length = len(nums)
+        left = 0
+        right = length - 1
+        while left < right:
+            if nums[right][1] + nums[left][1] > target:
+               right -= 1
+            elif nums[right][1] + nums[left][1] < target:
+                left += 1
             else:
-                hashTable[num] = indx
+                return [nums[right][0], nums[left][0]]
+
             
