449c6fd43d20b8caa7ebf88c1bcdd16a80ba136a
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-02-11 00:05:15 +0300

    Time: 73 ms (47.26%), Space: 13.9 MB (55.00%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -1,7 +1,6 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
         totalSum = (n + 1)*n//2
-        print(totalSum)
         prefixSum = [0]*(n + 1)
         for i in range(1, n + 1):
             prefixSum[i] += prefixSum[i - 1] + i
