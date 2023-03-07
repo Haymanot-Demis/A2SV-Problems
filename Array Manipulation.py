00fda756a12c4616ae5f570a7657a7dc76f63c5f
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-03-07 15:58:36 +0300

    added Array Manipulation.py

--- Array Manipulation.py
+++ Array Manipulation.py
@@ -1,3 +1,13 @@
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
 def arrayManipulation(n, queries):
     # Write your code here
     array = [0.0] * n
