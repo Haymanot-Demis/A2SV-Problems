d817a22114f76bed507c344ce800cf60fccc4aa3
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-03-09 09:40:22 +0300

    added binary-subarrays-with-sum.python

--- binary-subarrays-with-sum.py
+++ binary-subarrays-with-sum.py
@@ -1,12 +1,17 @@
+from collections import defaultdict
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
         hashmap = defaultdict(int)
-        hashmap[0] = 1
+        hashmap[nums[0]] = 1
         counter = 0
-        counter += hashmap[nums[0] - goal]
-        hashmap[nums[0]] += 1
+        if nums[0] == goal:
+            counter += 1
+        
         for i in range(1, len(nums)):
-            nums[i] += nums[i - 1]
-            counter += hashmap[nums[i] - goal]
+            nums[i] += nums[i-1]
+            if nums[i] == goal:
+                counter += 1
+            if hashmap[nums[i] - goal] != 0:
+                counter += hashmap[nums[i] - goal]
             hashmap[nums[i]] += 1
         return counter
