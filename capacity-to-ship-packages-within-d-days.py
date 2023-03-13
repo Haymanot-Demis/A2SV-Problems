d53cf36969fc4de83f54571a2627809156bbf21f
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-03-13 06:12:05 +0300

    added capacity-to-ship-packages-within-d-days.python

--- capacity-to-ship-packages-within-d-days.py
+++ capacity-to-ship-packages-within-d-days.py
@@ -1,25 +1,26 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        def canBeShipped(capacity, length, high, low):
-            days = 0
+        def canBeShipped(capacity, expected):
+            days = 1
             indx = 0
-            while indx < length:
-                weight = 0
-                while indx < length and weight + weights[indx] <= capacity: 
-                    weight += weights[indx]
-                    indx += 1
-                days += 1
-            
+            curr_weight = 0
+            for weight in weights:
+                if curr_weight + weight > capacity:
+                    days += 1
+                    curr_weight = 0
+                curr_weight += weight
+                if days > expected:
+                    return days
+
             return days
 
-        length = len(weights)
         low = max(weights)
         high = sum(weights)
         min_possible_weight = high
 
         while low <= high:
             mid = low + (high - low) // 2
-            days_to_ship = canBeShipped(mid, length, high, low)
+            days_to_ship = canBeShipped(mid, days)
 
             if days_to_ship > days:
                 low = mid + 1
