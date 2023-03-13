3b66b7724249924c162dcb3d75dfcdd7d45a9e43
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-03-13 06:33:54 +0300

    added koko-eating-bananas.python

--- koko-eating-bananas.py
+++ koko-eating-bananas.py
@@ -1,22 +1,15 @@
 class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def isFeasible(k, h):
-            hours = 0
-            for pile in piles:
-                if pile <= k:
-                    hours += 1
-                else:
-                    hours += ceil(pile / k)
-                if hours > h:
-                    return False
-            return True
-
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:         
         low = 1
         high = max(piles)
+
         while low <= high:
             mid = low + (high - low) // 2
-            if isFeasible(mid, h):
+            hours = 0
+            hours += sum([ceil(pile / mid) for pile in piles])
+            if hours <= h:
                 high = mid - 1
             else:
                 low = mid + 1
+
         return low
