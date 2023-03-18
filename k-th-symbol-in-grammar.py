7c6804bd2c0b08f4e882fec99e4c6db4afaa766e
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-03-18 14:17:22 +0300

    added k-th-symbol-in-grammar.python

--- k-th-symbol-in-grammar.py
+++ k-th-symbol-in-grammar.py
@@ -1,12 +1,15 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
+        """
+        the key of this problem is the first and last half of the string is inverted form of one an other
+        """
         if n == 1:
             return 0
         if n == 2:
             return int("01"[k - 1])
  
-        if k > 2 ** (n - 3) * 2:
-            prevK = self.kthGrammar(n - 1, k - 2 ** (n - 3) * 2)
+        if k > 2 ** (n - 2):
+            prevK = self.kthGrammar(n - 1, k - 2 ** (n - 2))
             return int(not(prevK))
         prevK = self.kthGrammar(n - 1, k) 
         return prevK
