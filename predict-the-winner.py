e6c1c7caba7ce81340cc92a15be372de4051e224
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-03-18 14:18:25 +0300

    added predict-the-winner.python

--- predict-the-winner.py
+++ predict-the-winner.py
@@ -1,18 +1,23 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        @cache    
-        def isWinner(l, r, turn):
-            if l > r:
-                return 0
+        #I think we have to test every path in which the first person can go through
+        first, second = self.IsWinner(nums, 0, len(nums) - 1, 0, 0, True)
+        return first >=  second
 
-            if turn:
-                left = nums[l] + isWinner(l + 1, r, not turn)
-                right = nums[r] + isWinner(l, r - 1, not turn)
-                return max(left, right)
+    def IsWinner(self, nums, l, r, first, second, turn):
+        if r < l:
+            return [first, second]
+        if turn:
+            f1,s1 = self.IsWinner(nums, l+1, r, first + nums[l], second, not turn)
+            f2,s2 = self.IsWinner(nums, l, r - 1, first + nums[r], second, not turn)
+            if f1 >= f2:
+                return [f1, s1]
             else:
-                left = -nums[l] + isWinner(l + 1, r, not turn)
-                right = -nums[r] + isWinner(l, r - 1, not turn)
-                return min(left, right)
-
-        res = isWinner(0, len(nums) - 1, True)
-        return res >=  0
+                return [f2, s2]
+        else:
+            f1,s1 = self.IsWinner(nums, l+1, r, first, second + nums[l], not turn)
+            f2,s2 = self.IsWinner(nums, l, r - 1, first, second + nums[r], not turn)
+            if s1 >= s2:
+                return [f1, s1]
+            else:
+                return [f2, s2]
