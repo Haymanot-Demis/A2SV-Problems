b8dcd57e5aa467e7cc28ad4878f5e745e70fa00e
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-03-20 17:13:51 +0300

    added h-index-ii.python

--- h-index-ii.py
+++ h-index-ii.py
@@ -2,18 +2,7 @@ class Solution:
     def hIndex(self, citations: List[int]) -> int:
         def isFeasible(hindex):
             left_pos = bisect_left(citations, hindex)
-            right_pos = bisect_right(citations, hindex)
-            length = len(citations)
-            while left_pos <= right_pos:
-                mid = left_pos + (right_pos - left_pos) // 2
-                if length - mid > hindex:
-                    left_pos = mid + 1
-                elif length - mid < hindex:
-                    right_pos = mid - 1
-                else:
-                    return 0
-                    
-            if length - right_pos > hindex:
+            if len(citations) - left_pos >= hindex:
                 return 1
             else:
                 return -1
@@ -24,11 +13,9 @@ class Solution:
         while low <= high:
             mid = low + (high - low) // 2
             res = isFeasible(mid)
-            if res == 0:
+            if res == 1:
                 ans = mid
-                high = mid - 1
-            elif res == - 1:
-                high = mid - 1
-            else:
                 low = mid + 1
+            else:
+                high = mid - 1
         return ans
