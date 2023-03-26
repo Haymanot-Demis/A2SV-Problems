2338595a57fb88ce3da7e072853120a30d6092bd
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-03-26 05:06:09 +0300

    added kth-largest-element-in-an-array.python

--- kth-largest-element-in-an-array.py
+++ kth-largest-element-in-an-array.py
@@ -1,3 +1,27 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        return sorted(nums, reverse=True)[k - 1]
+        def quicksort(nums, left, right, k):
+            if left >= right:
+                return
+            l = left
+            r = right
+            pivot = left
+            while left < right:
+                if pivot == right:
+                    while left < pivot and nums[pivot] <= nums[left]:
+                        left += 1
+                    nums[pivot], nums[left] = nums[left], nums[pivot]
+                    pivot = left                    
+                else:
+                    while right > pivot and nums[pivot] >= nums[right]:
+                        right -= 1
+                    nums[pivot], nums[right] = nums[right], nums[pivot]
+                    pivot = right   
+            if left == right == k - 1:
+                return nums[k - 1]  
+            left_subarr = quicksort(nums, l, pivot - 1, k)
+            right_subarr = quicksort(nums, pivot + 1, r, k)
+            return nums[k - 1]
+        if len(nums) == 1:
+            return nums[0]
+        return quicksort(nums, 0, len(nums) - 1, k)
