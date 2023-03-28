ed579b615b04c81b8c64bcdbc489603bc82073d3
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-03-28 18:16:33 +0300

    added missing-number.python

--- missing-number.py
+++ missing-number.py
@@ -1,13 +1,17 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        length  = len(nums) + 1
-        nums.append(length)
-        i = 0
-        while i < length:
-            if nums[i] != i and nums[i] != length:
-                temp = nums[i]
-                nums[i] = nums[nums[i]]
-                nums[temp] = temp
+        length  = len(nums)
+        pointer = 0
+        while pointer < length:
+            if nums[pointer] == pointer:
+                nums[pointer] = -1
+                pointer += 1
+            elif nums[pointer] < length and nums[pointer] != -1:
+                nums[nums[pointer]], nums[pointer] = -1, nums[nums[pointer]]
             else:
-                i += 1
-        return nums.index(length)
+                pointer += 1
+        
+        for indx in range(len(nums)):
+            if nums[indx] != -1:
+                return indx
+        return length
