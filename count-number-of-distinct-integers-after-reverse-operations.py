92ad1e2d937730c7a5dd9bacb8a10e97f04834a8
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-03-29 12:41:49 +0300

    added count-number-of-distinct-integers-after-reverse-operations.python

--- count-number-of-distinct-integers-after-reverse-operations.py
+++ count-number-of-distinct-integers-after-reverse-operations.py
@@ -3,14 +3,8 @@ class Solution:
         length = len(nums)
         indx = 0
         while indx < length:
-            num = nums[indx]
-            reverse = 0
-            while num >= 10:
-                unit = num % 10
-                num = num // 10
-                reverse = reverse * 10 + unit
-            reverse = reverse * 10 + num
+            reverse = int(str(nums[indx])[::-1])
             nums.append(reverse)
             indx += 1
-            
+
         return len(set(nums))
