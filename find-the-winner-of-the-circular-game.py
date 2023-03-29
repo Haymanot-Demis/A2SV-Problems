6ffa401b3a5338248d8a972250bb54c79e276f5c
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-03-29 11:54:20 +0300

    added find-the-winner-of-the-circular-game.python

---
+++ find-the-winner-of-the-circular-game.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        players = [i for i in range(1, n+1)]
+        index = 0
+        while len(players) > 1:
+            iterationCounter = 1
+            while iterationCounter < k and len(players) > 1:
+                index += 1
+                iterationCounter += 1
+            players.pop(index % len(players))
+            index = index % (len(players) + 1)
+        return int(players[0])
