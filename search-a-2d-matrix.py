09a697241ee626fcc6ae43ee84efe4a1da5d9337
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-03-29 11:55:23 +0300

    added search-a-2d-matrix.python

---
+++ search-a-2d-matrix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if target < matrix[0][0] or target > matrix[-1][-1]:
+            return False
+        
+        rowLength = len(matrix)
+        colLength = len(matrix[0])
+        rowToSearch = 0
+        for row in range(rowLength):
+            if target <= matrix[row][colLength - 1]:
+                rowToSearch = row
+                break
+        
+    
+        isFound = target in matrix[rowToSearch]
+        return isFound
