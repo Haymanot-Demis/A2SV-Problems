82baaf2616307a4e58fd20531acb7e2950cb4f70
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-04-02 10:30:51 +0300

    added subsets.python

--- subsets.py
+++ subsets.py
@@ -2,10 +2,17 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         def backtrack(indx, subset, subsets):
             if indx == len(nums):
-                subsets.append(subset.copy())
+                elems = []
+                i = 0 
+                while subset:
+                    if subset & 1:
+                        elems.append(nums[i])
+                    subset >>= 1
+                    i += 1
+                subsets.append(elems)
                 return 
-            backtrack(indx + 1, subset + [nums[indx]], subsets)
+            backtrack(indx + 1, subset | (1 << indx), subsets)
             backtrack(indx + 1, subset, subsets)
         subsets = []
-        backtrack(0, [], subsets)
+        backtrack(0, 0, subsets)
         return subsets
