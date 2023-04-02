34b6d8db24e392043040ab5d5025c7f99f239fef
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-04-02 15:42:44 +0300

    added power-of-two.python

--- power-of-two.py
+++ power-of-two.py
@@ -2,7 +2,9 @@ class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         if n <= 0:
             return False
-        if n == 1:
-            return True
-        
-        return self.isPowerOfTwo(n/2)
+
+        one_bits_count = 0 # counting the number of 1's in the binary representation
+        while n:
+            one_bits_count += 1
+            n = n & (n - 1)
+        return True if one_bits_count == 1 else False
