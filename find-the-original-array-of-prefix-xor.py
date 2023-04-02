badc3c605fe60cf70c38fa2e643ba9a657d5149c
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-04-02 17:34:28 +0300

    added find-the-original-array-of-prefix-xor.python

---
+++ find-the-original-array-of-prefix-xor.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        arr = pref.copy()
+        for i in range(1, len(pref)):
+            arr[i] ^= pref[i - 1]
+        return arr
