0c6c611e7292e720e30ec650430d1c9e5a0800cc
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-04-03 06:47:34 +0300

    added boats-to-save-people.python

---
+++ boats-to-save-people.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        length = len(people)
+        left = 0
+        right = length - 1
+        count = 0
+        people.sort(reverse=True)
+        # if length == 1:
+        #     return 1
+        while left <= right:
+            if left == right:
+                count +=1
+                break
+            if people[left] + people[right] <= limit:
+                left += 1
+                right -= 1
+            else:
+                left += 1
+            count +=1
+        return count
