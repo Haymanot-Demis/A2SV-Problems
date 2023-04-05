40900f644ccbe75db3b7f15f72e79687cc513aa3
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-04-05 19:49:48 +0300

    added count-primes.python

--- count-primes.py
+++ count-primes.py
@@ -2,21 +2,11 @@ class Solution:
     def countPrimes(self, n: int) -> int:
         if n <= 1:
             return 0
-        def isPrime(x):
-            if x <= 1:
-                return False
-            d = 2
-            while d <= int(x ** 0.5):
-                print("r", x % d)
-                if x % d == 0:
-                    return False
-                d += 1
-            return True
-        primes = [True] * (n - 1)
-        for i in range(1, int(n ** 0.5) + 1):
-            if isPrime(i):
+        primes = [True] * (n)
+        primes[0] = primes[1] = False
+        for i in range(2, int(n ** 0.5) + 1):
+            if primes[i]:
                 for j in range(i * i, n, i):
-                    primes[j - 1] = False
-            else:
-                primes[i - 1] = False
+                    primes[j] = False
+
         return primes.count(True)
