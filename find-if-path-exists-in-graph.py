f179d89cb7df91a3155d0f122e10eae65b24e0f2
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-04-07 18:30:07 +0300

    added find-if-path-exists-in-graph.python

--- find-if-path-exists-in-graph.py
+++ find-if-path-exists-in-graph.py
@@ -1,17 +1,18 @@
 class Solution:
+    def __init__(self):
+        self.adj_list = defaultdict(list)
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        adj_list = defaultdict(list)
         for u, v in edges:
-            adj_list[u].append(v)
-            adj_list[v].append(u)
-        return self.dfs(source, adj_list, set(), destination)
-    def dfs(self, node, adj_list, visited, dest):
+            self.adj_list[u].append(v)
+            self.adj_list[v].append(u)
+        return self.dfs(source, set(), destination)
+    def dfs(self, node, visited, dest):
         if node == dest:
             return True
         visited.add(node)
-        for v in adj_list[node]:
+        for v in self.adj_list[node]:
             if v not in visited:
-                if self.dfs(v, adj_list, visited, dest):
+                if self.dfs(v, visited, dest):
                     return True
                 
         return False
