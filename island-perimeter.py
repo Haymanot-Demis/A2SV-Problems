a145e32fb41fff1844197733f3e4c5a1785f27cb
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-04-07 19:42:57 +0300

    added island-perimeter.python

---
+++ island-perimeter.py
@@ -0,0 +1,31 @@
+class Solution:
+    def __init__(self):
+        self.grid = None
+        self.visited = None
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        self.grid = grid
+        self.visited = [ [False for _ in range(len(self.grid[0]))] for k in range(len(self.grid)) ]
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if self.grid[i][j]:
+                    return self.dfs(i, j)
+            
+    def dfs(self, row, col):
+        directions = [(0,1),(0,-1),(1,0),(-1, 0)]
+        def inbound(i, j):
+            return (0 <= i < len(self.grid)) and (0 <= j < len(self.grid[0]))
+        if not self.grid[row][col]:
+            return 1
+
+        self.visited[row][col] = True
+        count = 0
+        for horiz, vertic in directions:
+            new_row = row + horiz
+            new_col = col + vertic
+            if not inbound(new_row, new_col):
+                count += 1
+            if inbound(new_row, new_col) and not self.visited[new_row][new_col]:
+                count += self.dfs(new_row, new_col)
+            
+        return count
