858ad8a07cf33c7b3add6be7f54094a9e3ad8f7a
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-04-12 10:31:51 +0300

    added middle-of-the-linked-list.python

---
+++ middle-of-the-linked-list.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        p1 = head
+        p2 = head
+        count = 1
+        while p2 != None and p2.next != None:
+           p2 = p2.next.next
+           p1 = p1.next
+           count += 1
+        return p1
