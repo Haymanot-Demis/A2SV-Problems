8b726fa187cb8f268bc6d81786fe5275d6d21b58
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-04-25 14:56:23 +0300

    added last-stone-weight.python

---
+++ last-stone-weight.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        if len(stones) == 1:
+            return stones[0]
+        stones.sort()
+        while stones[-2]:
+            stones[-1] -= stones[-2]
+            stones[-2] = 0
+            stones.sort()
+        return stones[-1]
