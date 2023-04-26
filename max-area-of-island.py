7a00d001dc8a6e9e487a269733e3f298bf2c7a21
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-04-26 13:32:01 +0300

    added max-area-of-island.python

--- max-area-of-island.py
+++ max-area-of-island.py
@@ -1,24 +1,19 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         max_area = 0
-        print(grid)
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j]:
-                    stack = []
-                    stack.append((i, j))
-                    area = 0
-                    directions = [(0, -1), (-1, 0), (0, 1), (1, 0)]
-                    while stack:
-                        curr = stack.pop()
-                        area += grid[curr[0]][curr[1]]
-                        grid[curr[0]][curr[1]] = 0
-                        for x, y in directions:
-                            new_x = x + curr[0]
-                            new_y = y + curr[1]
-                            inbound = 0 <=  new_x < len(grid) and 0 <= new_y < len(grid[0])
-                            if inbound and grid[new_x][new_y]:
-                                stack.append((new_x, new_y))
-                    
-                    max_area = max(max_area, area)
+                    max_area = max(max_area, self.dfs(grid, i, j))
         return max_area
+    def dfs(self, grid, row, col):
+        grid[row][col] = 0
+        area = 1
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        for x, y in directions:
+            new_x = x + row
+            new_y = y + col
+            inbound = 0 <=  new_x < len(grid) and 0 <= new_y < len(grid[0])
+            if inbound and grid[new_x][new_y]:
+                area += self.dfs(grid, new_x, new_y)
+        return area
