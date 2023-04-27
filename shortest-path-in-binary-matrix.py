cbe20913373536be6494fc79bb8913c33d107693
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-04-27 19:48:33 +0300

    added shortest-path-in-binary-matrix.python

--- shortest-path-in-binary-matrix.py
+++ shortest-path-in-binary-matrix.py
@@ -5,21 +5,19 @@ class Solution:
         return self.BFS(grid, 0, 0)
     def BFS(self, grid, row, col):        
         directions = [(0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1)]
-        queue = deque([(0, 0)])
+        queue = deque([(0, 0, 1)])
         grid[0][0] = 1
-        shortest_path = 0
+
         while queue:
-            shortest_path += 1
-            length = len(queue)
-            for i in range(length):
-                curr = queue.popleft()
-                if curr[0] == len(grid) - 1 and curr[1] == len(grid[0]) - 1:
-                    return shortest_path
-                for i, j in directions:
-                    new_row = curr[0] + i
-                    new_col = curr[1] + j
-                    inbound = 0 <= new_row < len(grid) and 0 <= new_col < len(grid[0])
-                    if inbound and grid[new_row][new_col] == 0:
-                        queue.append((new_row, new_col))
-                        grid[new_row][new_col] = 1
+
+            curr = queue.popleft()
+            if curr[0] == len(grid) - 1 and curr[1] == len(grid[0]) - 1:
+                return curr[2]
+            for i, j in directions:
+                new_row = curr[0] + i
+                new_col = curr[1] + j
+                inbound = 0 <= new_row < len(grid) and 0 <= new_col < len(grid[0])
+                if inbound and grid[new_row][new_col] == 0:
+                    queue.append((new_row, new_col, curr[2] + 1))
+                    grid[new_row][new_col] = 1
         return -1
