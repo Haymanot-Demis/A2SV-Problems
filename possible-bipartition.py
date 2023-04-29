c3f150e43f18100ac823ce1a0edaacefc5f250a6
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-04-29 05:40:19 +0300

    added possible-bipartition.python

--- possible-bipartition.py
+++ possible-bipartition.py
@@ -3,24 +3,25 @@ class Solution:
         dislikes_list = defaultdict(list)
         for u,v in dislikes:
             dislikes_list[u].append(v)
+            dislikes_list[v].append(u)
 
         visited = set()
         nodes = list(dislikes_list.keys())
         for u in nodes: 
             if (u, 1) in visited or (u, 0) in visited:
                 continue
-            stack = [(u, 1)]
-            visited = set((u, 1))
-            
-
-            while stack:
-                node, color = stack.pop()
-                for adj in dislikes_list[node]:
-                    if (adj, color) in visited:
-                        return False
-                    new_color = 0 if color else 1
-                    if (adj, new_color) not in visited:
-                        visited.add((adj, new_color))
-                        stack.append((adj, new_color))
+            if not self.dfs(dislikes_list, u, 1, visited):
+                return False
+        return True
+    def dfs(self, dislikes_list, node, color, visited):
+        visited.add((node, color))
+        
+        for adj in dislikes_list[node]:
+            if (adj, color) in visited:
+                return False
+            new_color = 0 if color else 1
+            if (adj, new_color) not in visited:
+                if not self.dfs(dislikes_list, adj, new_color, visited):
+                    return False
 
         return True
