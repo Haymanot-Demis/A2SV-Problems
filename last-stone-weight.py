bf2a94ae74ebf5040624a896352135df8a872621
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-05-04 05:02:54 +0300

    added last-stone-weight.python

--- last-stone-weight.py
+++ last-stone-weight.py
@@ -1,10 +1,16 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
+        """
+        the concept bbehind this is build a max heap then heappop the top 2 largest numbers and subtract them if the difference is more than 0 heappushit again, do this until one or no value remained in the list, then return the result, we should map the stones to -ve to make it max heap
+        """
         if len(stones) == 1:
             return stones[0]
-        stones.sort()
-        while stones[-2]:
-            stones[-1] -= stones[-2]
-            stones[-2] = 0
-            stones.sort()
-        return stones[-1]
+        stones = list(map(lambda x : -x, stones))
+        heapify(stones)
+        while len(stones) > 1:
+            max1 = heappop(stones)
+            max2 = heappop(stones)
+            if max1 - max2:
+                heappush(stones, max1 - max2)
+                
+        return -stones[0] if stones else 0
