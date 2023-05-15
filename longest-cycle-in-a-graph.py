d168cc66b0da0e3266c34a7fb61f048f45e50847
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-05-15 17:11:51 +0300

    added longest-cycle-in-a-graph.python

--- longest-cycle-in-a-graph.py
+++ longest-cycle-in-a-graph.py
@@ -7,25 +7,25 @@ class Solution:
                 graph[u].append(v)
         max_cycle_len = -1
         position = [0] * len(edges)
+        
         for node in range(len(edges)):
             if not color[node]:
                 res = self.detectCycle(graph, node, color, 0, position)
-                if res:
-                    max_cycle_len = max(max_cycle_len, res)
+                max_cycle_len = max(max_cycle_len, res)
         return max_cycle_len
+
     def detectCycle(self, graph, node, color, length, position):
         if color[node] == 2:
             # not cycle
-            return
+            return -1
         if color[node] == 1:
             return length - position[node]
         color[node] = 1
         position[node] = length
         max_cycle_len = -1
-        for adj in graph[node]:
-            result = self.detectCycle(graph, adj, color, length + 1, position)
-            if result:
-                max_cycle_len = max(max_cycle_len, result)
+        if len(graph[node]):
+            result = self.detectCycle(graph, graph[node][0], color, length + 1, position)
+            max_cycle_len = max(max_cycle_len, result)
         
         color[node] = 2
         return max_cycle_len
