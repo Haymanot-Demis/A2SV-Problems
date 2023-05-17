d677aba8654e1aaa9a0eecf2400b2c79017450c5
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-05-17 17:50:20 +0300

    added check-if-there-is-a-valid-path-in-a-grid.python

--- check-if-there-is-a-valid-path-in-a-grid.py
+++ check-if-there-is-a-valid-path-in-a-grid.py
@@ -1,20 +1,42 @@
 class Solution:
-    def hasValidPath(self, grid: List[List[int]]) -> bool:
-        return self.dfs(grid, 0, 0)
-    def dfs(self, grid, row, col):
-        directions = {1:[(0, -1), (0, 1)], 2:[(-1, 0), (1, 0)], 3:[(0, -1),(1, 0)], 4:[(1, 0), (0, 1)], 5:[(0, -1), (-1, 0)], 6:[(-1, 0), (0, 1)]}
-        if row == len(grid) - 1 and col == len(grid[0]) - 1:
-            return True
-        curr = grid[row][col]
-        grid[row][col] = 0
-        # valid_neighbour = {1:set()}
+    def init_union_find(self, n, m):
+        self.group = {}
+        self.size = defaultdict(lambda : 1)
+        for i  in range(n):
+            for j in range(m):
+                self.group[(i, j)]= (i, j)
+    def find(self, pos):
+        if pos == self.group[pos]:
+            return pos
+        self.group[pos] = self.find(self.group[pos])
+        return self.group[pos]
+    
+    def union(self, A, B):
+        gr_A = self.find(A)
+        gr_B = self.find(B)
 
-        for i , j in directions[curr]:
-            new_row = row + i
-            new_col = col + j
-            inbound = 0 <= new_row < len(grid)  and 0 <= new_col < len(grid[0])
-            if inbound and grid[new_row][new_col] and (-i, -j) in directions[grid[new_row][new_col]]:
-                if self.dfs(grid, new_row, new_col):
-                    return True
-        grid[row][col]=curr 
-        return False
+        if gr_A != gr_B:
+            if self.size[gr_A] >= self.size[gr_B]:
+                self.group[gr_B] = gr_A
+                self.size[gr_A] += self.size[gr_B]
+            else:
+                self.group[gr_A] = gr_B
+                self.size[gr_B] += self.size[gr_A]
+    def areConnected(self, A, B):
+        return self.find(A) == self.find(B)
+    
+    def hasValidPath(self, grid: List[List[int]]) -> bool:
+        n = len(grid)
+        m = len(grid[0])
+        self.init_union_find(n, m)
+        directions = {1:[(0, -1), (0, 1)], 2:[(-1, 0), (1, 0)], 3:[(0, -1),(1, 0)], 4:[(1, 0), (0, 1)], 5:[(0, -1), (-1, 0)], 6:[(-1, 0), (0, 1)]}
+
+        for row in range(n):
+            for col in range(m):
+                for i , j in directions[grid[row][col]]:
+                    new_row = row + i
+                    new_col = col + j
+                    inbound = 0 <= new_row < n and 0 <= new_col < m
+                    if inbound and grid[new_row][new_col] and (-i, -j) in directions[grid[new_row][new_col]]:
+                        self.union((row, col), (new_row, new_col))
+        return self.areConnected((0, 0), (n - 1, m - 1))
