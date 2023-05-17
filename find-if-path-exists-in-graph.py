cbd4d45abc290181fba986bebfd1acd42744e112
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-05-17 06:09:58 +0300

    added find-if-path-exists-in-graph.python

--- find-if-path-exists-in-graph.py
+++ find-if-path-exists-in-graph.py
@@ -1,18 +1,38 @@
 class Solution:
     def __init__(self):
-        self.adj_list = defaultdict(list)
+        self.rep = {}
+
+    def init_union_find(self, size):
+        self.rep = {i:i for i in range(size)}
+        self.size = [1] * size
+
+    def find(self, u):
+        if u == self.rep[u]:
+            return u
+        self.rep[u] = self.find(self.rep[u])
+        return  self.rep[u]
+    
+    def union(self, u, v):
+        rep_u = self.find(u)
+        rep_v = self.find(v)
+
+        if rep_u != rep_v:
+            if self.size[rep_u] >= self.size[rep_v]:
+                self.rep[rep_v] = rep_u
+                self.size[rep_u] += self.size[rep_v]
+            else:
+                self.rep[rep_u] = rep_v
+                self.size[rep_v] += self.size[rep_u]
+    def areConnected(self, u, v):
+        rep_u = self.find(u)
+        rep_v = self.find(v)
+
+        return rep_u == rep_v
+
+
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        self.init_union_find(n)
         for u, v in edges:
-            self.adj_list[u].append(v)
-            self.adj_list[v].append(u)
-        return self.dfs(source, set(), destination)
-    def dfs(self, node, visited, dest):
-        if node == dest:
-            return True
-        visited.add(node)
-        for v in self.adj_list[node]:
-            if v not in visited:
-                if self.dfs(v, visited, dest):
-                    return True
-                
-        return False
+            self.union(u, v)
+        
+        return self.areConnected(source, destination)
