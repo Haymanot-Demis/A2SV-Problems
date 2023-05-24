bb8a4e6d4a01dd58c1b6e18c52a5cd9efdf53ad4
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-05-24 22:49:54 +0300

    added lemonade-change.python

---
+++ lemonade-change.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        counter = defaultdict(int)
+        for bill in bills:
+            counter[bill] += 1
+            if bill == 10:
+                if counter[5] == 0:
+                    return False
+                counter[5] -= 1
+            elif bill == 20:
+                if counter[5] and counter[10]:
+                    counter[5] -= 1
+                    counter[10] -= 1
+                elif counter[5] >= 3:
+                    counter[5] -= 3
+                else:
+                    return False
+        return True
