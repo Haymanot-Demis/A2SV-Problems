a9964eaab3a4dc714360967e31f51227dd4d646c
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-05-28 14:01:31 +0300

    added climbing-stairs.python

--- climbing-stairs.py
+++ climbing-stairs.py
@@ -6,8 +6,6 @@ class Solution:
         return self.help(n, memo)
         
     def help(self, n: int, memo) -> int:
-        if n <= 1:
-            return n
         if n in memo:
             return memo[n]
         memo[n] = self.help(n - 1, memo) + self.help(n - 2, memo)
