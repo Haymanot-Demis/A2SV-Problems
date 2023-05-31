58963e956e409e0c53e5475bbee73aff78234cb3
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-05-31 18:13:24 +0300

    added fibonacci-number.python

--- fibonacci-number.py
+++ fibonacci-number.py
@@ -2,10 +2,4 @@ class Solution:
     def fib(self, n: int) -> int:
         if n <= 1:
             return n
-        fibonacci = [0]*(n + 1)
-        fibonacci[0], fibonacci[1] = 0, 1
-
-        for state in range(2, n + 1):
-            fibonacci[state] = fibonacci[state - 1] + fibonacci[state - 2]  
-
-        return fibonacci[n]
+        return self.fib(n - 1) + self.fib(n - 2)
