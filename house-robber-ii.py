2f42c9595ec2200bd181fbac834b19f4e61a40e1
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-06-02 17:37:41 +0300

    added house-robber-ii.python

--- house-robber-ii.py
+++ house-robber-ii.py
@@ -1,22 +1,18 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo = {}
-        res = self.backtrack(nums, 0, 0, memo, [], False)
-        return res
+        if len(nums) == 1:
+            return nums[0]
+        one = self.dp(nums[:-1])
+        two = self.dp(nums[1:])
 
-    def backtrack(self, nums, indx, money, memo, store, flag=False):
-        if indx >= len(nums):
-            return money
+        return max(one, two)  
+
+    def dp(self, nums):
+        pprev, prev = 0, nums[0]
+
+        for i in range(1, len(nums)):
+            temp = prev 
+            prev = max(pprev + nums[i], prev)
+            pprev = temp
         
-        if indx == len(nums) - 1 and flag:
-            return money
-        
-        if (indx, flag, money) not in memo:
-            if indx == 0:
-                m1 = self.backtrack(nums, indx + 2, money + nums[indx], memo, store + [nums[indx]],True)
-            else:
-                m1 = self.backtrack(nums, indx + 2, money + nums[indx], memo, store + [nums[indx]],flag)
-            m2 = self.backtrack(nums, indx + 1, money, memo, store,flag)
-            memo[(indx, flag, money)] = max(m1, m2)
-        
-        return memo[(indx, flag, money)]
+        return prev
