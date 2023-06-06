15b5e042b5b2ddec18be7e49d60bdf7011173ad2
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-06-06 08:49:36 +0300

    added decode-ways.python

---
+++ decode-ways.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        ways_to_decode = [0] * (len(s) + 1)
+        ways_to_decode[-1] = 1
+        if s[-1] != "0":
+            ways_to_decode[-2] = 1
+        for i in range(len(s) - 2, -1, -1):
+            if s[i] != "0" and 1 <= int(s[i:i+2]) <= 26:
+                ways_to_decode[i] = ways_to_decode[i + 1] + ways_to_decode[i + 2]
+            elif s[i] == "0":
+                ways_to_decode[i] = 0
+            else:
+                ways_to_decode[i] = ways_to_decode[i + 1]
+
+        print(ways_to_decode)
+        return ways_to_decode[0]
