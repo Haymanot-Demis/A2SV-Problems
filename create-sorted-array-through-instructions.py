dcd37ab568dad0a7f33b0a3c25bc76c85282f072
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-06-11 12:47:43 +0300

    added create-sorted-array-through-instructions.python

--- create-sorted-array-through-instructions.py
+++ create-sorted-array-through-instructions.py
@@ -1,18 +1,19 @@
+from sortedcontainers import SortedList
 class Solution:
     def createSortedArray(self, instructions: List[int]) -> int:
-        nums = []
+        nums = SortedList([])
         counter = defaultdict(int)
 
         total_cost = 0
 
         for num in instructions:
-            pos = bisect_right(nums, num)
+            pos = nums.bisect_right(num)
 
             smallers = pos - counter[num]
             greaters = len(nums) - pos
 
             counter[num] += 1
-            nums.insert(pos, num)
+            nums.add(num)
 
             cost = min(smallers, greaters)
             total_cost += cost
