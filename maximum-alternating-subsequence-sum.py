86344bb5e5c6dc32d0be8e3d42d63b47d86faeec
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-07-03 19:44:13 +0300

    added maximum-alternating-subsequence-sum.python

--- maximum-alternating-subsequence-sum.py
+++ maximum-alternating-subsequence-sum.py
@@ -1,12 +1,9 @@
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
-        memo = [0] * len(nums)
-
         even, odd = 0, nums[0]
 
         for i in range(len(nums)):
-            memo[i] = max(even + nums[i], odd - nums[i], odd)
             even = max(even, odd - nums[i])
             odd = max(odd, even + nums[i])
 
-        return memo[-1]
+        return max(even, odd)
