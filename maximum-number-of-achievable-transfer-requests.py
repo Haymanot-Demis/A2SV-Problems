9c74b15e997494777b96b8b85ce6937df8ef35ad
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-07-03 06:29:41 +0300

    added maximum-number-of-achievable-transfer-requests.python

--- maximum-number-of-achievable-transfer-requests.py
+++ maximum-number-of-achievable-transfer-requests.py
@@ -1,19 +1,23 @@
 class Solution:
     def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
-        def backtrack(indx, count, net_transfer):
+        def backtrack(indx, net_transfer, memo):
             if indx >= len(requests):
                 if net_transfer.count(0) == len(net_transfer):
                     return 0
                 return -inf
+            
+            if (indx, net_transfer) in memo:
+                return memo[(indx, net_transfer)]
+        
+            net_transfer1 = list(net_transfer)
+            net_transfer1[requests[indx][0]] -= 1
+            net_transfer1[requests[indx][1]] += 1
 
-            net_transfer[requests[indx][0]] -= 1
-            net_transfer[requests[indx][1]] += 1
-            one = backtrack(indx + 1, count + 1, net_transfer)  
+            one = backtrack(indx + 1, tuple(net_transfer1), memo)  
+            two = backtrack(indx + 1, net_transfer, memo)
 
-            net_transfer[requests[indx][0]] += 1
-            net_transfer[requests[indx][1]] -= 1         
-            two = backtrack(indx + 1, count, net_transfer)
+            memo[(indx, net_transfer)] = max(one + 1, two)
 
             return max(one + 1, two)
 
-        return backtrack(0, 0, [0] * n)
+        return backtrack(0, tuple([0] * n), defaultdict(int))
