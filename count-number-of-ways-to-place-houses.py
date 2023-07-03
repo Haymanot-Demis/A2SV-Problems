2395d95b67bfb1bb7a3b71bd007779bf70de2b15
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-07-03 16:51:55 +0300

    added count-number-of-ways-to-place-houses.python

--- count-number-of-ways-to-place-houses.py
+++ count-number-of-ways-to-place-houses.py
@@ -1,16 +1,10 @@
 class Solution:
     def countHousePlacements(self, n: int) -> int:
-        if n == 1:
-            return 4
-        
-        if n == 2:
-            return 9
-
-        pprev = 2
-        prev = 3
+        pprev = 1
+        prev = 2
         modulo = 10 ** 9 + 7
 
-        for i in range(2, n):
+        for i in range(1, n):
             prev += pprev
             pprev = prev - pprev
 
