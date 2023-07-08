ada34c5ce52baa0391c37cc9847018ea8343c9e2
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-07-08 06:01:16 +0300

    added maximize-the-confusion-of-an-exam.python

--- maximize-the-confusion-of-an-exam.py
+++ maximize-the-confusion-of-an-exam.py
@@ -1,13 +1,14 @@
 class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        """
+        Think about how many consecutive T's or F's can we make until the current index by building a prefixsum for either T's or F's
+        """
         prefixsum = [0] * len(answerKey)
         prefixsum[0] += answerKey[0] == 'T'
 
         for i in range(1, len(answerKey)):
             prefixsum[i] += prefixsum[i - 1] + (answerKey[i] == 'T')
 
-        print(prefixsum)
-        
         max_confusion = 0
         Thash = defaultdict(lambda : -1, {0:-1})
         Fhash = defaultdict(lambda : -1, {0:-1})
@@ -15,8 +16,9 @@ class Solution:
         for i in range(len(prefixsum)):
             trues = prefixsum[i]
             falses = i + 1 - prefixsum[i]
-            F = i - Thash[trues - k] # if we we want to change them to T
-            T = i - Fhash[falses - k] # if we want to change to F        
+            F = i - Thash[trues - k] # if we we want to change them to F
+            T = i - Fhash[falses - k] # if we want to change to T       
+
             if trues not in Thash:
                 Thash[trues] = i
 
