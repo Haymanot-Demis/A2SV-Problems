6199ee027b04c2dd560f53a003136504bcf4bd45
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-07-25 18:04:59 +0300

    added best-time-to-buy-and-sell-stock-with-cooldown.python

--- best-time-to-buy-and-sell-stock-with-cooldown.py
+++ best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,22 +1,42 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # bottom up
         memo = {}
-        def dp(indx, state):
-            # state can be sell(1), buy(2) or cooldown(3)
-            if (indx,state) in memo:
-                return memo[(indx, state)]
+        for i in range(len(prices)):
+            temp_memo = dict(memo)
+            temp_memo[(i, 1)] = -prices[i]
+            temp_memo[(i, 3)] = 0
+            temp_memo[(i, 2)] = 0
+
+            for key, profit in memo.items():
+                indx, state = key
+                if state == 1:
+                    temp_memo[(i, 2)] = max(temp_memo[(i, 2)], profit + prices[i])
+                elif state == 3:
+                    temp_memo[(i, 1)] = max(temp_memo[(i, 1)], profit - prices[i])
+                else:
+                    temp_memo[(i, 3)] = max(temp_memo[(i, 3)], profit)
+                
+            memo = temp_memo
+        return max(memo.values())
+
+        # Top down
+        # def dp(indx, state):
+        #     # state can be sell(1), buy(2) or cooldown(3)
+        #     if (indx,state) in memo:
+        #         return memo[(indx, state)]
             
-            if indx >= len(prices):
-                return 0
+        #     if indx >= len(prices):
+        #         return 0
 
-            profit = 0
-            if state == 1:    
-                profit = max(profit, dp(indx + 1, state))
-                profit = max(profit, dp(indx + 1, 2) - prices[indx])
-            else:
-                profit = max(profit, dp(indx + 1, state))
-                profit = max(profit, dp(indx + 2, 1) + prices[indx])
-            memo[(indx, state)] = profit
+        #     profit = 0
+        #     if state == 1:    
+        #         profit = max(profit, dp(indx + 1, state))
+        #         profit = max(profit, dp(indx + 1, 2) - prices[indx])
+        #     else:
+        #         profit = max(profit, dp(indx + 1, state))
+        #         profit = max(profit, dp(indx + 2, 1) + prices[indx])
+        #     memo[(indx, state)] = profit
 
-            return profit
-        return dp(0, 1)
+        #     return profit
+        # return dp(0, 1)
