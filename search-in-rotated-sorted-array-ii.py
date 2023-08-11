3da2c56d3be0efdaeeaf293555d3e98061644b2d
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-08-11 05:06:21 +0300

    added search-in-rotated-sorted-array-ii.python

---
+++ search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,37 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        if not nums:
+            return False
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[left] == target or nums[mid] == target or nums[right] == target:
+                return True
+            if nums[left] == nums[mid] == nums[right]:
+                return self.search(nums[:mid], target) or self.search(nums[mid + 1:], target)
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target <= nums[mid]:
+                    return self.binarySearch(nums, left, mid, target)
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] <= target <= nums[right]:
+                    return self.binarySearch(nums, mid, right, target)
+                else:
+                    right = mid - 1
+        return False
+
+    def binarySearch(self, nums, left, right, target):
+        print(left, right)
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return True
+        
+        return False
