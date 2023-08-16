a0e1983713ae991193029a597ef5dc2447389e00
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-08-16 05:58:34 +0300

    added power-of-four.python

--- power-of-four.py
+++ power-of-four.py
@@ -1,7 +1,11 @@
+#time 5
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        if n == 1 or n == 1.0:
-            return True
-        elif n%4 != 0 or n <= 0:
+        if n <= 0:
             return False
-        return self.isPowerOfFour(n/4)
+        bit_len = n.bit_length()
+
+        if n.bit_count() == 1: # it must have only one set bit
+            print("dsf")
+            return bit_len % 2 == 1 # it is power of two of power of two number so the position must be even but since bit starts from zero from right it must be odd to be power of 4
+        return False
