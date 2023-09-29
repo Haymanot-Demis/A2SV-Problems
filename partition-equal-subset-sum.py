b603cc0a498a1c3c6c81ff33d02b9f051c11da65
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-09-29 11:15:09 +0300

    added partition-equal-subset-sum.python

--- partition-equal-subset-sum.py
+++ partition-equal-subset-sum.py
@@ -1,13 +1,28 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if sum(nums) % 2 != 0:
-            return False
-        memo = set([nums[0], -nums[0]])
+        memo = set()
+        total = sum(nums)
 
-        for i in range(1, len(nums)):
-            temp_memo = set()
+        if total % 2 == 1:
+            return False
+
+        half = total // 2
+
+        for i in range(len(nums)):
+            temp = memo.copy()
+            temp.add(nums[i])
             for value in memo:
-                temp_memo.add(value + nums[i])     
-                temp_memo.add(value - nums[i])
-            memo = temp_memo
-        return 0 in memo
+                _sum = value + nums[i]
+
+                if _sum == half:
+                    return True
+                
+                if _sum < half:
+                    temp.add(_sum)
+            
+            if half in temp:
+                return True
+
+            memo = temp
+
+        return False
