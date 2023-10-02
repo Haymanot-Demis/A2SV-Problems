774f55589a427c8111d53a1772c27af9d8598dc7
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-10-02 14:41:58 +0300

    added search-suggestions-system.python

--- search-suggestions-system.py
+++ search-suggestions-system.py
@@ -1,17 +1,48 @@
+class TrieNode:
+    def __init__(self):
+        self.kids = {}
+        self.indices = []
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()    
+
+    def insert(self, word, indx) -> None:
+        node = self.root
+
+        for ch in word:
+            if ch not in node.kids:
+                node.kids[ch] = TrieNode()
+            
+            node = node.kids[ch]
+            if len(node.indices) < 3:
+                node.indices.append(indx)
+    def prefixSearch(self, prefix):
+        node = self.root
+
+        for ch in prefix:
+            if ch not in node.kids:
+                return []
+            node = node.kids[ch]
+            
+        return node.indices
+
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        trie = Trie()
         products.sort()
-        answer = []
-        start = 0
-        for i in range(len(searchWord)):
-            prefix = searchWord[:i + 1]
-            start = bisect_left(products, prefix, start)
-            result = []
-            for j in range(start, min(len(products), start + 3)):
-                k = min(len(prefix), len(products[j]))
-                if products[j][:k] == prefix:
-                    result.append(products[j])
-            
-            answer.append(result)
+        for indx, prod in enumerate(products):
+            trie.insert(prod, indx)
         
-        return answer
+        suggestion = []
+        prefix = ""
+        for ch in searchWord:
+            prefix += ch
+            res = trie.prefixSearch(prefix)
+            w = []
+            for indx in res:
+                w.append(products[indx])
+                
+            suggestion.append(w)
+        
+        return suggestion
