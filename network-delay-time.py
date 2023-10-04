6f12a639c2bd0abbcc959186b4e874bd9e91d050
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-10-04 11:47:00 +0300

    added network-delay-time.python

--- network-delay-time.py
+++ network-delay-time.py
@@ -1,5 +1,8 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        '''
+        start from the starting node go to its children, if they have better time now add it to the queue, always take the node with samllest time taken from the queue.
+        '''
         adj_list = defaultdict(list)
         for u, v, w in times:
             adj_list[u].append((v, w))
@@ -7,17 +10,17 @@ class Solution:
         signalReachedAt = [inf] * n
         signalReachedAt[k - 1] = 0
 
-        queue = deque([(k, 0)])
+        minheap = [(0, k)]
 
-        while queue:
-            u, time = queue.popleft()
+        while minheap:
+            time, u = heappop(minheap)
 
             for adj in adj_list[u]:
                 v, w = adj
                 if signalReachedAt[v - 1] > time + w:
-                    queue.append((v , time + w))
+                    heappush(minheap, (time + w, v))
                     signalReachedAt[v - 1] = time + w
-        
+
         if inf  in signalReachedAt:
             return -1
 
