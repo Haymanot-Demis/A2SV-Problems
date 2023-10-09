bff2753900456c55d7626cd07313715309eff43e
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-10-09 11:26:18 +0300

    added knight-dialer.python

--- knight-dialer.py
+++ knight-dialer.py
@@ -1,5 +1,7 @@
 class Solution:
     def knightDialer(self, n: int) -> int:
+        if n == 1:
+            return 10
         neighbours = {
             1:[6, 8], 
             2:[7, 9], 
@@ -15,15 +17,12 @@ class Solution:
 
         memo = defaultdict(int, {(1, 1) : 1, (2, 1) : 1, (3, 1):1, (4, 1):1, (5, 1):1, (6, 1):1, (7, 1):1, (8, 1):1, (9, 1):1, (0, 1):1})
 
-        def dp(key, n):
-            if (key, n) in memo:
-                return memo[(key, n)]
-            
-            for adj in neighbours[key]:
-                memo[(key, n)] += dp(adj, n - 1)
-            
-            return memo[(key, n)]
         res = 0
-        for i in range(10):
-            res += dp(i, n)
+        for j in range(2, n + 1):
+            for i in range(10):
+                for adj in neighbours[i]:
+                    memo[(i, j)] += memo[(adj, j - 1)]
+                if j == n:
+                    res += memo[(i, j)]
+
         return res % (10 ** 9 + 7)
