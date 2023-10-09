0161914835692ff79f80440af3efa094571b2253
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-10-09 09:43:16 +0300

    added cheapest-flights-within-k-stops.python

--- cheapest-flights-within-k-stops.py
+++ cheapest-flights-within-k-stops.py
@@ -5,41 +5,23 @@ class Solution:
         for u, v, w in flights:
             graph[u].append((v, w))
 
-        heap = [(0, 0, src)]
-        distance_arr = [inf] * n
-        distance_arr[src] = 0
-        k = k
-        
-        while heap:
-            stops, cost, node = heappop(heap)
+        queue = deque([(0, src)])
+        distance_arr = [inf] * n  
+        distance_arr[src] = 0   
+        k = k + 1
 
-            if stops <= k:
+        while k:
+            length = len(queue)
+
+            for _ in range(length):
+                cost, node = queue.popleft()  
+
                 for adj, w in graph[node]:
                     if distance_arr[adj] > cost + w:
                         distance_arr[adj] = cost + w
-                        heappush(heap, (stops + 1, cost + w, adj))
-
-        return distance_arr[dst] if distance_arr[dst] != inf else -1
-
-# class Solution:
-#     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-#         graph = defaultdict(list)
-#         for start, dest, price in flights:
-#             graph[start].append((dest, price))
+                        queue.append((cost + w, adj))  
+            k -= 1
         
-#         prices = [float('inf') for i in range(n)]
-#         prices[src] = 0
-        
-#         minHeap = [( 0, prices[src], src)]
-        
-#         while minHeap:
-#             stops, total_cost, cur = heappop(minHeap)
-#             neighbours = graph[cur]
-            
-#             for neighbour, cost in neighbours:
-#                 if prices[neighbour] > total_cost + cost and stops <= k: 
-#                     prices[neighbour] = total_cost + cost
-                    
-#                     heappush(minHeap, (stops + 1, prices[neighbour], neighbour))
-
-#         return prices[dst] if prices[dst] != float('inf') else -1
+        if distance_arr[dst] == inf:
+            return -1
+        return distance_arr[dst]
