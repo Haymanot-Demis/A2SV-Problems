fec59d97e8ef7b930fbedfbf9f1306f6440a9388
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-10-12 16:12:47 +0300

    added find-the-index-of-the-first-occurrence-in-a-string.python

--- find-the-index-of-the-first-occurrence-in-a-string.py
+++ find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,25 +1,38 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if len(haystack) < len(needle):
-            return -1
+        LPS = [0] * len(needle)
 
-        p = len(needle) - 1
+        def KMP_LPS():
+            j = 0
+            i = 1
+            while i < len(needle):
+                if needle[i] == needle[j]:
+                    LPS[i] = j + 1
+                    j += 1
+                    i += 1
+                else:
+                    if j == 0:
+                        i += 1
+                    else:
+                        j = LPS[j - 1]
+        KMP_LPS()
 
-        hashed = 0
-        windw = 0
-        for i in range(p + 1):
-            hashed += (ord(needle[i]) - 96) * (26 ** (p - i))
-            windw += (ord(haystack[i]) - 96) * (26 ** (p - i)) 
+        j = 0
+        i = 0
+        l1 = len(haystack)
+        l2 = len(needle)
         
-        if hashed == windw:
-            return 0
+        while i < l1 and j < l2:
+            if needle[j] != haystack[i]:
+                if j > 0:
+                    j = LPS[j - 1]
+                else:
+                    i += 1
+            else:
+                i += 1
+                j += 1
         
-        for j in range(p + 1, len(haystack)):
-            windw -= (ord(haystack[j - (p + 1)]) - 96) * (26 ** (p))
-            windw *= 26
-            windw += (ord(haystack[j]) - 96)
+        if j == l2:
+            return i - l2
 
-            if hashed == windw:
-                return j - p
-            
         return -1
