7635062cb1cd785a554134b41bf3cfbb5921748a
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-10-14 09:28:43 +0300

    added min-stack.python

---
+++ min-stack.py
@@ -0,0 +1,27 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = list()
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        if len(self.stack):
+            return self.stack.pop()
+        return None      
+
+    def getMin(self) -> int:
+        if len(self.stack):
+            minVal = self.top()
+            for num in self.stack:
+                if minVal > num:
+                    minVal = num
+            return minVal
+        return None
+
+            
+    def top(self) -> int:
+        if len(self.stack):
+            return self.stack[-1]
+        return None
