a08da1cb126d61c0a589d3ce003287fe1a367ad8
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-10-14 09:23:08 +0300

    added permutation-in-string.python

--- permutation-in-string.py
+++ permutation-in-string.py
@@ -1,19 +1,15 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        srcHash = dict(collections.Counter(s1))
-        windowSize = len(s1)
-        windowEnd = windowSize - 1
-        windowStart = 0
-        length = len(s2)
-        permutation = s2[windowStart:windowEnd]
-        permutationHash = dict(collections.Counter(permutation))
-        while windowEnd < length:
-            permutationHash[s2[windowEnd]] = permutationHash.get(s2[windowEnd],0) + 1
-            if permutationHash == srcHash:
-               return True
-            permutationHash[s2[windowStart]] -= 1
-            if permutationHash[s2[windowStart]] == 0:
-                permutationHash.pop(s2[windowStart])
-            windowStart += 1
-            windowEnd += 1
+        needleHash = Counter(s1)
+        windwHash = Counter(s2[:len(s1) - 1])
+
+        for i in range(len(s1) - 1, len(s2)):
+            windwHash[s2[i]] += 1
+
+            if needleHash == windwHash:
+                return True
+            windwHash[s2[i + 1 - len(s1)]] -= 1
+            if windwHash[s2[i + 1 - len(s1)]] == 0:
+                del windwHash[s2[i + 1 - len(s1)]]
+            
         return False
