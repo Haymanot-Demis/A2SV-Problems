99258b13a038a7d2c59375651dc82fe2234a771d
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-10-18 22:38:36 +0300

    added lexicographically-smallest-equivalent-string.python

--- lexicographically-smallest-equivalent-string.py
+++ lexicographically-smallest-equivalent-string.py
@@ -1,26 +1,28 @@
 class Solution:
     def __init__(self):
-        self.group = {chr(i + 97) : chr(i + 97)  for i in range(26)}
-    def find(self, pos):
-        if pos == self.group[pos]:
-            return pos
-        self.group[pos] = self.find(self.group[pos])
-        return self.group[pos]
+        self.rep = [i for i in range(26)]
+    def find(self, x):
+        if x != self.rep[x]:
+            r = self.find(self.rep[x]) 
+            self.rep[x] = r
+        return self.rep[x]
     
-    def union(self, A, B):
-        gr_A = self.find(A)
-        gr_B = self.find(B)
+    def union(self, x, y):
+        rep_x = self.find(x)
+        rep_y = self.find(y)
 
-        if gr_A != gr_B:
-            if gr_B >= gr_A:
-                self.group[gr_B] = gr_A
-            else:
-                self.group[gr_A] = gr_B
-
+        if rep_x <= rep_y:
+            self.rep[rep_y] = rep_x
+        else:
+            self.rep[rep_x] = rep_y
+        
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
         for i in range(len(s1)):
-            self.union(s1[i], s2[i])
+            self.union(ord(s1[i]) - 97, ord(s2[i]) - 97)
+        
         ans = ""
-        for i in range(len(baseStr)):
-            ans += self.find(baseStr[i])
+
+        for ch in baseStr:
+            ans += chr(self.find(ord(ch) - 97) + 97)
+        
         return ans
