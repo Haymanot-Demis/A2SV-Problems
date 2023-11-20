2f98053047705531dafd1930c7d861cccea9e8ef
Haymanot Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2023-11-20 15:39:35 +0300

    added decode-string.python

--- decode-string.py
+++ decode-string.py
@@ -1,29 +1,20 @@
 class Solution:
-    """
-    go from right most of the given string to left
-    if it is alphabetical add to the answer 
-    if it is closing bracket we have to compute inner expression so call the recursive funtion
-    if is opening bracket the next is number so take the number and mutiply the string and continue parsing
-    """
     def decodeString(self, s: str) -> str:
-        s = list(s)
-        decodeString = self.decodeStringHelper(s)
-        return ''.join(decodeString[::-1])
+        stack = []
 
-    def decodeStringHelper(self, stack:List[str]):
-        ans = ""
-        number = ""
-        while stack:
-            if stack[-1].isalpha():
-                ans += stack.pop()
-            elif stack[-1] == "]":
+        for ch in s:
+            if ch  == "]":
+                encoded_string = []
+                while stack and stack[-1] != "[":
+                    encoded_string.append(stack.pop())
+
                 stack.pop()
-                ans += self.decodeStringHelper(stack)
-            else: # it is nopening bracke and next numeric
-                if stack[-1] == "[": stack.pop()# remove the opening bracket
-                while stack and stack[-1].isnumeric():
-                    number = stack.pop() + number
-                print(number)
-                ans = ans * int(number)
-                return ans
-        return ans
+                k = ""
+                while stack and stack[-1].isdigit():
+                    k += stack.pop()
+                k = int(k[::-1])
+                stack.append("".join(encoded_string[::-1] * k))
+            else:
+                stack.append(ch)
+
+        return "".join(stack)
